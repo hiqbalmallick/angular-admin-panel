ca08c0c3f293c19e5d5f8a9cbfe1abfb2e388951
Hamza Iqbal <hiqbalmallick@gmail.com>
2021-07-13 14:45:51 +0500

    introduced login guard

---
+++ src/app/guards/login/login.guard.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  Router,
+  RouterStateSnapshot,
+  UrlTree,
+} from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class LoginGuard implements CanActivate {
+  constructor(private router: Router) {}
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ):
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree>
+    | boolean
+    | UrlTree {
+    const user = JSON.parse(JSON.stringify(localStorage.getItem('user')));
+    if (user) {
+      this.router.navigate(['/']);
+      return false;
+    }
+    return true;
+  }
+}
